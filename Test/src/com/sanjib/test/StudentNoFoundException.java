2f8251d8ee576e4cd9b23bfd5953a1223d37fbe4
sanjibgit19 <55386654+sanjibgit19@users.noreply.github.com>
2021-06-14 11:10:08 +0530

    Test
    commited by sanjib

--- Test/src/com/sanjib/test/StudentNoFoundException.java
+++ Test/src/com/sanjib/test/StudentNoFoundException.java
@@ -3,7 +3,7 @@ package com.sanjib.test;
 public class StudentNoFoundException extends RuntimeException {
 	
 	public StudentNoFoundException() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	public StudentNoFoundException(String msg) {
